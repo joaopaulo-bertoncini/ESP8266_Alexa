028034295986ba74027f7b6620eef150aa0d959d
João Paulo S Bertoncini <joao.bertoncini@gmail.com>
2022-12-14 09:36:09 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ESP8266_Alexa
+# ESP8266 and Alexa
+## This project shows how to integrate IoT devices like lamps and outlets by controlling them remotely by voice using Alexa.
+
+### NodeMCU ESP8266
+<br/>
+<img src="resource/esp8266.png">
+
+### The circuit
+<br/>
+<img src="resource/circuit.jpeg">
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,99 @@
 #include <Arduino.h>
-#include "network.h"
+#ifdef ARDUINO_ARCH_ESP32
+#include <WiFi.h>
+#else
+#include <ESP8266WiFi.h>
+#endif
+#include <Espalexa.h>
 
-#define RELAY_PIN_1 5
-#define LAMP "Lâmpada 1"
+// prototypes
+boolean connectWifi();
 
-void setup() {
+//callback functions
+void firstLightChanged(uint8_t brightness);
+
+// Change this!!
+const char* ssid = "JPSB";
+const char* password = "";
+
+boolean wifiConnected = false;
+
+Espalexa espalexa;
+
+EspalexaDevice* device1;
+
+#define RelayPin1 5  //D1
+
+void setup()
+{
   Serial.begin(9600);
-  scanNetworks();
-  printWiFiData();
+  pinMode(RelayPin1, OUTPUT);
+  // Initialise wifi connection
+  wifiConnected = connectWifi();
+  if(wifiConnected){
+    device1 = new EspalexaDevice("Light1", firstLightChanged); //you can also create the Device objects yourself like here
+    espalexa.addDevice(device1); //and then add them
+    device1->setValue(255); //this allows you to e.g. update their state value at any time!
+    espalexa.begin();
+  } else
+  {
+    while (1) {
+      Serial.println("Cannot connect to WiFi. Please check data and reset the ESP.");
+      delay(2500);
+    }
+  }
+}
+ 
+void loop()
+{
+   espalexa.loop();
+   delay(1);
 }
 
-void loop() {
+//our callback functions
+void firstLightChanged(uint8_t brightness) {
+    //Control the device
+    if (brightness == 255)
+      {
+        digitalWrite(RelayPin1, HIGH);
+        Serial.println("Device1 ON");
+      }
+    else if (brightness == 0)
+    {
+      digitalWrite(RelayPin1, LOW);
+      Serial.println("Device1 OFF");
+    }
+}
 
+// connect to wifi – returns true if successful or false if not
+boolean connectWifi(){
+  boolean state = true;
+  int i = 0;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 20){
+      state = false; break;
+    }
+    i++;
+  }
+  Serial.println("");
+  if (state){
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    Serial.println("Connection failed.");
+  }
+  return state;
 }
